9312dc153cddd0029c5edf00415e04bfdf2fc320
AVANADE-CORP\renan.de.souza.pinto <renan.de.souza.pinto@avanade.com>
2018-08-31 20:44:11 -0300

    adicionado modal paciente

--- src/Pacientes.js
+++ src/Pacientes.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import './pacientes.css';
 import $ from 'jquery';
 import { Scrollbars } from 'react-custom-scrollbars';
+import PubSub from 'pubsub-js';
+import Modal from 'react-responsive-modal';
 import MenuLateral from './components/Menu';
 import TopMenu from './components/TopMenu';
 import FormCadastroPaciente from './components/FormCadastroPaciente';
-import PubSub from 'pubsub-js';
+// import './custom-animation.css';
 
 
 class Home extends Component {
@@ -15,7 +17,10 @@ class Home extends Component {
       lista: [],
       isHidden: true,
       phone: '',
+      open: false,
     };
+    // this.onOpenModal=this.onOpenModal.bind(this);
+    this.onCloseModal=this.onCloseModal.bind(this)
   }
 
   componentDidMount() {
@@ -41,23 +46,34 @@ class Home extends Component {
     }.bind(this));
   }
 
+
+  onOpenModal() {
+    console.log('open', this.state.open);
+    this.setState({ open: true });
+  }
+
+  onCloseModal() {
+    this.setState({ open: false });
+  }
+
   toggleHidden() {
     console.log('hidden', this.state.isHidden);
     this.setState({isHidden: !this.state.isHidden })
   }
 
   render() {
+    const { open } = this.state;
     let formularioPaciente;
     if (!this.state.isHidden) {
       formularioPaciente = <FormCadastroPaciente/>
     } 
 
-    const listByName = this.state.lista.slice(0);
-    listByName.sort(function(a,b) {
-    const x = a.name.toLowerCase();
-    const y = b.name.toLowerCase();
-    return x < y ? -1 : x > y ? 1 : 0;
-    });
+    // const listByName = this.state.lista.slice(0);
+    // listByName.sort(function(a,b) {
+    // const x = a.name.toLowerCase();
+    // const y = b.name.toLowerCase();
+    // return x < y ? -1 : x > y ? 1 : 0;
+    // });
 
 
     return (
@@ -99,12 +115,37 @@ class Home extends Component {
          
          {formularioPaciente}
          
-          <section className="person-boxes">
+          <div className="example">
+        {/* <h4>Custom animation</h4>
+        <button className="btn btn-action" onClick={this.onOpenModal.bind(this)}>
+        Open
+      </button>{' '} */}
+      <Modal
+          open={open}
+          onClose={this.onCloseModal}
+          center
+          classNames={{
+            transitionEnter: 'transition-enter',
+            transitionEnterActive: 'transition-enter-active',
+            transitionExit: 'transition-exit-active',
+            transitionExitActive: 'transition-exit-active',
+          }}
+          animationDuration={500}
+        >
+          <p>
+          <FormCadastroPaciente/>
+          </p>
+</Modal>
+
+</div>
+      <section className="person-boxes">
               {
-               listByName.map(function(paciente){
+               this.state.lista.map(function(paciente){
                 console.log('paciente', paciente.name);
                 return (
-                    <div className="person-box">
+                  <div>
+                  <button class="btn-modal" onClick={this.onOpenModal.bind(this)}>
+                  <div className="person-box">
                       <div className="box-avatar">
                         <img src="http://icons.iconarchive.com/icons/papirus-team/papirus-status/512/avatar-default-icon.png" alt={paciente.name}/>
                       </div>
@@ -115,24 +156,27 @@ class Home extends Component {
                       </div>
                       {/* <div className="box-actions">
                         <button>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.855 14.365l-1.817 6.36a1.001 1.001 0 0 0 1.517 1.106L12 18.202l5.445 3.63a1 1 0 0 0 1.517-1.106l-1.817-6.36 4.48-3.584a1.001 1.001 0 0 0-.461-1.767l-5.497-.916-2.772-5.545c-.34-.678-1.449-.678-1.789 0L8.333 8.098l-5.497.916a1 1 0 0 0-.461 1.767l4.48 3.584zm2.309-4.379c.315-.053.587-.253.73-.539L12 5.236l2.105 4.211c.144.286.415.486.73.539l3.79.632-3.251 2.601a1.003 1.003 0 0 0-.337 1.056l1.253 4.385-3.736-2.491a1 1 0 0 0-1.109-.001l-3.736 2.491 1.253-4.385a1.002 1.002 0 0 0-.337-1.056l-3.251-2.601 3.79-.631z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6.855 14.365l-1.817 6.36a1.001 1.001 0 0 0 1.517 1.106L12 18.202l5.445 3.63a1 1 0 0 0 1.517-1.106l-1.817-6.36 4.48-3.584a1.001 1.001 0 0 0-.461-1.767l-5.497-.916-2.772-5.545c-.34-.678-1.449-.678-1.789 0L8.333 8.098l-5.497.916a1 1 0 0 0-.461 1.767l4.48 3.584zm2.309-4.379c.315-.053.587-.253.73-.539L12 5.236l2.105 4.211c.144.286.415.486.73.539l3.79.632-3.251 2.601a1.003 1.003 0 0 0-.337 1.056l1.253 4.385-3.736-2.491a1 1 0 0 0-1.109-.001l-3.736 2.491 1.253-4.385a1.002 1.002 0 0 0-.337-1.056l-3.251-2.601 3.79-.631z"/></svg>
                         </button>
 
                         <button>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 18H6V6h7V4H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-8h-2v7z"/><path d="M17.465 5.121l-6.172 6.172 1.414 1.414 6.172-6.172 2.12 2.121L21 3h-5.657z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 18H6V6h7V4H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-8h-2v7z"/><path d="M17.465 5.121l-6.172 6.172 1.414 1.414 6.172-6.172 2.12 2.121L21 3h-5.657z"/></svg>
                         </button>
                 
                         <button>
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 3C6.486 3 2 6.364 2 10.5c0 2.742 1.982 5.354 5 6.678V21a.999.999 0 0 0 1.707.707l3.714-3.714C17.74 17.827 22 14.529 22 10.5 22 6.364 17.514 3 12 3zm0 13a.996.996 0 0 0-.707.293L9 18.586V16.5a1 1 0 0 0-.663-.941C5.743 14.629 4 12.596 4 10.5 4 7.468 7.589 5 12 5s8 2.468 8 5.5-3.589 5.5-8 5.5z"/></svg>
-                        </button>
-                        
-                        <button>
+                          </button>
+                          
+                          <button>
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 8L17 8 17 11 14 11 14 13 17 13 17 16 19 16 19 13 22 13 22 11 19 11zM3 20h10c.553 0 1-.447 1-1v-.5c0-2.54-1.212-4.651-3.077-5.729C11.593 12.063 12 11.1 12 10c0-2.28-1.72-4-4-4s-4 1.72-4 4c0 1.1.407 2.063 1.077 2.771C3.212 13.849 2 15.96 2 18.5V19C2 19.553 2.448 20 3 20zM6 10c0-1.178.822-2 2-2s2 .822 2 2-.822 2-2 2S6 11.178 6 10zM8 14c2.43 0 3.788 1.938 3.977 4H4.023C4.212 15.938 5.57 14 8 14z"/></svg>
+                          </button>
+                        </div> */}
+                      </div>
                         </button>
-                      </div> */}
-                    </div>
+                         </div>
                   );
-                })
+                 
+                }.bind(this))
               }
            </section>
           </Scrollbars>
