82d70d6fee899d0d94bece5b0079ee72ffcd4833
AVANADE-CORP\renan.de.souza.pinto <renan.de.souza.pinto@avanade.com>
2018-09-13 12:33:11 -0300

    login cm google

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import { GoogleLogin } from 'react-google-login';
 import './Login.css';
+
+const responseGoogle = (response) => {
+  console.log(response);
+};
 
 class Login extends React.Component {
   render() {
@@ -20,6 +25,12 @@ class Login extends React.Component {
           <p id="restore-password" className="pointer">Esqueceu a senha?</p>
         </div>
         <input type="submit" name="submit" value="LOGIN" className="submit"/>
+          <GoogleLogin
+          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          buttonText="Login com Google"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          />
         <div className="row row-text">
           <p id="login-switch" className="pointer">Cadastre-se</p>
         </div>
