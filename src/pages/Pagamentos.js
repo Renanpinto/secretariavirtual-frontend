67db50062368bba10abc554bcc1ac80e80e6dadb
Vinicius vini.loreto@gmail.com <Avanade@WIN-7T4VJ1E9ZU.corp.avanade.org>
2018-11-05 20:47:19 -0200

    alinhamento da busca

--- src/pages/Pagamentos.js
+++ src/pages/Pagamentos.js
@@ -4,6 +4,7 @@ import 'react-table/react-table.css';
 import '../css/react-table.css';
 import '../css/pagamentos.css';
 import $ from 'jquery';
+import { Grid } from '@material-ui/core';
 
 
 class Pagamentos extends Component {
@@ -11,9 +12,9 @@ class Pagamentos extends Component {
     super();
     this.state = {
       lista: [],
-      search:''
+      search: ''
     };
-    this.updateSearch = this.updateSearch.bind(this);
+    // this.updateSearch = this.updateSearch.bind(this);
   }
 
   componentDidMount() {
@@ -22,99 +23,108 @@ class Pagamentos extends Component {
       url: 'http://ema-api.herokuapp.com/api/customers',
       crossDomain: true,
       dataType: 'json',
-      success: function(resultado){
-        this.setState({lista: resultado})
+      success: function (resultado) {
+        this.setState({ lista: resultado })
       }.bind(this),
-      error: function(resultado) {
+      error: function (resultado) {
         console.log("deu ruim: ", resultado);
       }
 
     });
   }
 
-   updateSearch(event){
-    this.setState({search: event.target.value.substr(0,20)})
+  updateSearch(event) {
+    this.setState({ search: event.target.value.substr(0, 20) })
   }
 
- render() {
-  let filteredPayments = this.state.lista.filter(
-      (paciente) =>{
-        return paciente.name.toLowerCase().indexOf(this.state.search.toLowerCase())!== -1;
-      }            
+  render() {
+    let filteredPayments = this.state.lista.filter(
+      (paciente) => {
+        return paciente.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+      }
     );
 
-  return (
+    return (
 
-        <main className="content-wrap">
+      <main className="content-wrap">
 
-          <header className="content-head">
-            <h1>Pagamentos</h1>     
-            <div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"/></svg>
-            <input type="text" id='searchs' value={this.props.search} onChange={this.updateSearch.bind(this)}></input>          
-            </div>
-          </header>
-          <br></br>
-          
-              <div>
-                <ReactTable
-                  pageText= 'Pagina'
-                  ofText='de'
-                  nextText='Proxima'
-                  previousText='Anterior'
-                  rowsText='linhas'
-                  data={filteredPayments}
-                   
-                  columns={[
-                    {
-                      Header: "Paciente",
-                      columns: [
-                        {
-                          Header: "Nome",
-                          accessor: "name"
-                        },
-                        {
-                          Header: "E-Mail",
-                          accessor: "email"
-                        }
-                      ]
-                    },
-                    {
-                      Header: "Quantidade",
-                      columns: [
-                        {
-                          Header: "N de consultas",
-                          //só colocar o nome da propriedade
-                          accessor: "phone"
-                        }
-                      ]
-                    },
-                    {
-                      Header: "Pagamento",
-                      columns: [                      
-                        {
-                          Header: "Vencimento",
-                          accessor: "created_at"
-                        },
-                        {
-                          Header: "Valor R$",
-                          accessor: "updated_at"
-                        },
-                        {
-                          Header: "Status",
-                          id: "status",
-                          accessor: d => d.status.toString()
-                        }
-                      ]
-                    }
-                ]}
-                    defaultPageSize={10}
-                    className="-striped -highlight"
-                />
-                
-              </div>
-          
-        </main>
+        <header className="content-head">
+
+
+
+          <h1>Pagamentos</h1>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z" /></svg>
+            <input type="text" id='searchs' value={this.props.search} onChange={this.updateSearch.bind(this)}></input>
+          </Grid>
+
+        </header>
+        <br></br>
+
+        <div>
+          <ReactTable
+            pageText='Pagina'
+            ofText='de'
+            nextText='Proxima'
+            previousText='Anterior'
+            rowsText='linhas'
+            data={filteredPayments}
+
+            columns={[
+              {
+                Header: "Paciente",
+                columns: [
+                  {
+                    Header: "Nome",
+                    accessor: "name"
+                  },
+                  {
+                    Header: "E-Mail",
+                    accessor: "email"
+                  }
+                ]
+              },
+              {
+                Header: "Quantidade",
+                columns: [
+                  {
+                    Header: "N de consultas",
+                    //só colocar o nome da propriedade
+                    accessor: "phone"
+                  }
+                ]
+              },
+              {
+                Header: "Pagamento",
+                columns: [
+                  {
+                    Header: "Vencimento",
+                    accessor: "created_at"
+                  },
+                  {
+                    Header: "Valor R$",
+                    accessor: "updated_at"
+                  },
+                  {
+                    Header: "Status",
+                    id: "status",
+                    accessor: d => d.status.toString()
+                  }
+                ]
+              }
+            ]}
+            defaultPageSize={10}
+            className="-striped -highlight"
+          />
+
+        </div>
+
+      </main>
     );
   }
 }
