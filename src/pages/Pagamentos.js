917f7631cb406b40709e7564cce8fd916b550ae5
AVANADE-CORP\renan.de.souza.pinto <renan.de.souza.pinto@avanade.com>
2018-11-29 10:57:57 -0200

    mock pagamento

--- src/pages/Pagamentos.js
+++ src/pages/Pagamentos.js
@@ -38,11 +38,25 @@ class Pagamentos extends Component {
   }
 
   render() {
-    let filteredPayments = this.state.lista.filter(
-      (paciente) => {
-        return paciente.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
-      }
-    );
+    // let filteredPayments = this.state.lista.filter(
+    //   (paciente) => {
+    //     return paciente.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+    //   }
+    // );
+
+    // console.log("filteredPayments", filteredPayments)
+
+    const filteredPayments = [{
+      id: 1,
+      name: "Cliente A",
+      email: "cliente_a@email.com",
+      appointments: "4",
+      value: "440,00",
+      status: false,
+      created_at: "20/10/2018"
+    }]
+
+
 
     return (
 
@@ -52,12 +66,12 @@ class Pagamentos extends Component {
 
 
 
-          <h1>Pagamentos</h1>
+          <h1>Pagamentos a receber</h1>
           <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
+            // container
+            // // direction="row"
+            // justify="center"
+            // alignItems="center"
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z" /></svg>
             <input type="text" id='searchs' value={this.props.search} onChange={this.updateSearch.bind(this)}></input>
@@ -66,7 +80,7 @@ class Pagamentos extends Component {
         </header>
         <br></br>
 
-        <div>
+        <div id="tabela-pagamentos">
           <ReactTable
             pageText='Pagina'
             ofText='de'
@@ -77,7 +91,7 @@ class Pagamentos extends Component {
 
             columns={[
               {
-                Header: "Paciente",
+                Header: "Cliente",
                 columns: [
                   {
                     Header: "Nome",
@@ -90,30 +104,44 @@ class Pagamentos extends Component {
                 ]
               },
               {
-                Header: "Quantidade",
+                Header: "Dados da fatura",
                 columns: [
                   {
-                    Header: "N de consultas",
+                    Header: "Atendimentos",
                     //só colocar o nome da propriedade
-                    accessor: "phone"
+                    // accessor: "phone"
+                    accessor: "appointments"
+                  },
+                  {
+                    Header: "R$",
+                    //só colocar o nome da propriedade
+                    accessor: "value"
                   }
                 ]
               },
               {
-                Header: "Pagamento",
+                Header: "Valor em aberto: ",
                 columns: [
                   {
                     Header: "Vencimento",
                     accessor: "created_at"
                   },
                   {
-                    Header: "Valor R$",
-                    accessor: "updated_at"
-                  },
-                  {
                     Header: "Status",
                     id: "status",
-                    accessor: d => d.status.toString()
+                    // accessor: d => d.status === true ? "Vencido" : "A vencer",
+                    // accessor: d => d.status.toString()
+                    accessor: "status",
+                    Cell: row => (
+                    <span>
+                      <span style={{
+                        color: row.value === true ? '#ff2e00' : '#57d500',
+                      }}>
+                        &#x25cf;
+                      </span> {
+                        row.value === true ? 'Vencido': 'A vencer'
+                      }
+                    </span>)
                   }
                 ]
               }
