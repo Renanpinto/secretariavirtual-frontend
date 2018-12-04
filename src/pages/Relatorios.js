87a3699ba73e4975b0ac43d232636982a8722f88
AVANADE-CORP\renan.de.souza.pinto <renan.de.souza.pinto@avanade.com>
2018-12-04 09:46:22 -0200

    relatorio

--- src/pages/Relatorios.js
+++ src/pages/Relatorios.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import { CSVLink } from "react-csv";
-// const moment = require('moment');
+import moment from 'moment';
 
 class Relatorios extends Component {
     constructor() {
         super();
-        this.state = {
+        this.relatorio = {
             customers: [],
             appointments: [],
-            payments: []
+            payments: [],
         };
     }
 
@@ -20,7 +20,8 @@ class Relatorios extends Component {
             dataType: 'json',
             success: function (resultado) {
                 console.log(" Customers ", resultado)
-                this.setState({ customers: resultado })
+                // this.setState({ customers: resultado })
+                this.relatorio.customers.push(resultado);
             }.bind(this),
             error: function (resultado) {
                 console.log("deu ruim: ", resultado);
@@ -33,7 +34,7 @@ class Relatorios extends Component {
             dataType: 'json',
             success: function (resultado) {
                 console.log(" APPOINTMENTS ", resultado)
-                this.setState({ appointments: resultado })
+                this.relatorio.appointments = resultado;
             }.bind(this)
         });
 
@@ -43,41 +44,47 @@ class Relatorios extends Component {
             dataType: 'json',
             success: function (resultado) {
                 console.log(" Faturas ", resultado)
-                this.setState({ payments: resultado })
+                this.relatorio.payments = resultado;
             }.bind(this)
         });
     }
 
     
     render() {
+        
+        // if(this.relatorio.customers.length>0) {
+        //     console.log('asdas', this.relatorio)
 
-
-        this.state.customers.forEach( customers => {
+        this.relatorio.customers.forEach(customer => {
            //TODO -- da conflito com o status do payments, se deixar os 2 um nao pega
-            customers.status = customers.status === true ? "Ativo" : "Inativo";
+           console.log('assas') 
+           customer.status = customer.status === true ? "Ativo" : "Inativo";
+            console.log('aaa', customer.status)
 
             //TODO -- CRASHA O RESTO DAS DATAS
-            // customers.created_at = moment(customers.created_at).format("DD/MM/YYYY");
+            customer.created_at = moment(customer.created_at).format("DD/MM/YYYY");
         });
         
-        this.state.appointments.forEach( (appointments) => {
-            // if(appointments.start_time) {
-            //     appointments.start_time = moment(appointments.start_time).format("DD/MM/YYYY");
-            // }
-            // if(appointments.end_time){
-            //     appointments.end_time = moment(appointments.end_time).format("DD/MM/YYYY");   
-            // }                         
-            // if(appointments.status) {
+        this.relatorio.appointments.forEach( (appointment) => {
+            if(appointment.start_time) {
+                appointment.start_time = moment(appointment.start_time).format("DD/MM/YYYY");
+            }
+            if(appointment.end_time){
+                appointment.end_time = moment(appointment.end_time).format("DD/MM/YYYY");   
+            }                         
+            // if(appointment.status) {
             //     //TODO -- true = ??
-            //     appointments.status = appointments.status === true ? "...": "..."
+            //     appointment.status = appointment.status === true ? "...": "..."
             // }                                                                                                                                                                                                                                          
         })
 
-        this.state.payments.forEach( payments => {
+        this.relatorio.payments.forEach( payment => {
            //TODO -- da conflito com o status do costumers, se deixar os 2 um nao pega
-            // payments.status = payments.status === true ? "Pago" : "Em Aberto"
+            payment.status = payment.status === true ? "Pago" : "Em Aberto"
 
         })
+        console.log('relatorio ', this.relatorio)
+    // }
         
         let headersCustomers = [
             { label: "Id", key: "id" },
@@ -117,7 +124,7 @@ class Relatorios extends Component {
                             <div className="box-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21 20V4a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1zm-2-1H5V5h14v14z" /><path d="M10.381 12.309l3.172 1.586a1 1 0 0 0 1.305-.38l3-5-1.715-1.029-2.523 4.206-3.172-1.586a1.002 1.002 0 0 0-1.305.38l-3 5 1.715 1.029 2.523-4.206z" /></svg>
                             </div>
-                            <CSVLink filename={"relatorios-pagamentos.csv"} className="exportReport" data={this.state.payments} headers={headersPayments}>
+                            <CSVLink filename={"relatorios-pagamentos.csv"} className="exportReport" data={this.relatorio.payments} headers={headersPayments}>
                                 Relatorio de Pagamentos
                         </CSVLink>
                         </div>
@@ -127,7 +134,7 @@ class Relatorios extends Component {
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 10H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V11a1 1 0 0 0-1-1zm-1 10H5v-8h14v8zM5 6h14v2H5zM7 2h10v2H7z" /></svg>
                             </div>
 
-                            <CSVLink filename={"relatorio-consultas.csv"} className="exportReport" data={this.state.appointments} headers={headersAppointments}>
+                            <CSVLink filename={"relatorio-consultas.csv"} className="exportReport" data={this.relatorio.appointments} headers={headersAppointments}>
                                 Relatório de Consultas
                         </CSVLink>
                         </div>
@@ -136,7 +143,7 @@ class Relatorios extends Component {
                             <div className="box-icon">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3,21c0,0.553,0.448,1,1,1h16c0.553,0,1-0.447,1-1v-1c0-3.714-2.261-6.907-5.478-8.281C16.729,10.709,17.5,9.193,17.5,7.5 C17.5,4.468,15.032,2,12,2C8.967,2,6.5,4.468,6.5,7.5c0,1.693,0.771,3.209,1.978,4.219C5.261,13.093,3,16.287,3,20V21z M8.5,7.5 C8.5,5.57,10.07,4,12,4s3.5,1.57,3.5,3.5S13.93,11,12,11S8.5,9.43,8.5,7.5z M12,13c3.859,0,7,3.141,7,7H5C5,16.141,8.14,13,12,13z" /></svg>
                             </div>
-                            <CSVLink filename={"relatorio-clientes.csv"} className="exportReport" data={this.state.customers} headers={headersCustomers}>
+                            <CSVLink filename={"relatorio-clientes.csv"} className="exportReport" data={this.relatorio.customers} headers={headersCustomers}>
                                 Relatório de Clientes
                         </CSVLink>
 
